7538fe8fd1f9e6b7891c864f7caca13256716d04
Nikita_Suprunyuk <suprunyuk.nikita.gm@gmail.com>
2023-10-27 22:16:08 +0300

    data base part 1 new files

---
+++ BirthdayCalendar/Data/PersonsDB.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BirthdayCalendar.Models;
+using SQLite;
+
+namespace BirthdayCalendar.Data
+{
+    class PersonsDB
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public PersonsDB(string connectionString)
+        {
+            db = new SQLiteAsyncConnection(connectionString);
+
+            db.CreateTableAsync<Person>().Wait();
+        }
+
+        public Task<List<Person>> GetPersonsAsync()
+        {
+            return db.Table<Person>().ToListAsync();
+        }
+
+        public Task<Person> GetPersonAsync(int id)
+        {
+            return db.Table<Person>()
+                .Where(i => i.ID == id)
+                .FirstOrDefaultAsync();
+        }
+        
+        public Task<int> SavePersonAsync(Person person)
+        {
+            if(person.ID != 0)
+            {
+                return db.UpdateAsync(person);
+            }
+            else
+            {
+                return db.InsertAsync(person);
+            }
+        }
+
+        public Task<int> DeletePersonAsync(Person person)
+        {
+            return db.DeleteAsync(person);
+        }
+    }
+}
---
+++ BirthdayCalendar/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace BirthdayCalendar.Models
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string image { get; set; }
+        public DateTime date { get; set; }
+    }
+}
